afb3728aaf748c451cf5998bace939fc1e23f01a
gjayashree2000 <49433917+gjayashree2000@users.noreply.github.com>
2019-04-22 10:58:31 +0530

    Add files via upload

---
+++ dictionary la value append.py
@@ -0,0 +1,31 @@
+#create your own dictionary
+dic={}
+n=int(input("enter the no. of set"))
+for i in range (n):
+    #item=(input("enter the key and value separted by comma"))
+    #list1=item.split(',')
+    #key=list1[0]
+    #value=list1[1]
+    #dic[key]=value
+#print(dic)
+    key=int(input("enter the key"))
+    value=int(input("enter the value"))
+    dic[key]=value
+print(dic)
+
+#sort the key
+sorted(dic.keys())
+print(sorted(dic.keys()))
+
+#sort the value
+sorted(dic.values())
+print(sorted(dic.values()))
+
+user=int(input("enter key need to check"))
+if user in dic:
+    print("key is present")
+else:
+    print("invalid key")    
+
+
+   
